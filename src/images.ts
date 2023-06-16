62d8b86bea69f1725a95e81aa8d292f3890c4326
Aqamarine228 <MrFurgJK@gmail.com>
2023-06-16 10:34:04 +0200

    little changes

--- src/images.ts
+++ src/images.ts
@@ -3,7 +3,7 @@ export default {
 	graphics: {
 		neutral: `${__dirname}/../assets/images/graphicNeutral.svg`,
 		buy: `${__dirname}/../assets/images/graphicBuy.svg`,
-		strong_buy: `${__dirname}/../assets/images/graphicStrongSell.svg`,
+		strong_buy: `${__dirname}/../assets/images/graphicStrongBuy.svg`,
 		sell: `${__dirname}/../assets/images/graphicSell.svg`,
 		strong_sell: `${__dirname}/../assets/images/graphicStrongSell.svg`,
 	},
