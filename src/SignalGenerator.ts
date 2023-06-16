0dcf60758353358ee5e5c808b3b891cb719a7f3d
Aqamarine228 <MrFurgJK@gmail.com>
2023-06-16 10:07:52 +0200

    little changes

--- src/SignalGenerator.ts
+++ src/SignalGenerator.ts
@@ -3,40 +3,40 @@ import ImageGenerator from "./ImageGenerator";
 import images from "./images";
 
 export default class SignalGenerator extends ImageGenerator {
-	private readonly currency: string;
+  private readonly currency: string;
 
-	private mode: "sell" | "buy" | "neutral";
+  private mode: "sell" | "buy" | "neutral" | "strong_sell" | "strong_buy";
 
-	constructor(currency: string, mode: "sell" | "buy" | "neutral") {
-		super();
+  constructor(currency: string, mode: "sell" | "buy" | "neutral" | "strong_sell" | "strong_buy") {
+    super();
 		this.currency = currency;
-		this.mode = mode;
-	}
+    this.mode = mode;
+  }
 
-	protected generate(): Promise<Buffer> {
-		const { drawBackground } = this;
-		(async () => drawBackground(this.context))();
-		return this.drawGraphic().then(() => {
-			this.drawCurrency();
-			return this.canvas.toBuffer("image/png");
-		});
-	}
+  protected generate(): Promise<Buffer> {
+    const { drawBackground } = this;
+    (async () => drawBackground(this.context))();
+    return this.drawGraphic().then(() => {
+      this.drawCurrency();
+      return this.canvas.toBuffer("image/png");
+    });
+  }
 
-	private async drawBackground(context: typeof this.context) {
-		return loadImage(images.background).then((background) => {
-			context.drawImage(background, 0, 0, 1920, 1080);
-		});
-	}
+  private async drawBackground(context: typeof this.context) {
+    return loadImage(images.background).then((background) => {
+      context.drawImage(background, 0, 0, 1920, 1080);
+    });
+  }
 
-	private drawGraphic() {
-		return loadImage(images.graphics[this.mode]).then((graphic) => {
-			this.context.drawImage(graphic, 260, 500, 1400, 600);
-		});
-	}
+  private drawGraphic() {
+    return loadImage(images.graphics[this.mode]).then((graphic) => {
+      this.context.drawImage(graphic, 260, 500, 1400, 600);
+    });
+  }
 
-	private drawCurrency() {
-		this.context.font = '150px "SpaceGrotesk" semi-bold';
-		this.context.fillStyle = "#F3CC30";
-		this.context.fillText(this.currency, 180, 280);
-	}
+  private drawCurrency() {
+    this.context.font = "150px \"SpaceGrotesk\" semi-bold";
+    this.context.fillStyle = "#F3CC30";
+    this.context.fillText(this.currency, 180, 280);
+  }
 }
--- src/images.ts
+++ src/images.ts
@@ -3,6 +3,8 @@ export default {
 	graphics: {
 		neutral: `${__dirname}/../assets/images/graphicNeutral.svg`,
 		buy: `${__dirname}/../assets/images/graphicBuy.svg`,
+		strong_buy: `${__dirname}/../assets/images/graphicStrongSell.svg`,
 		sell: `${__dirname}/../assets/images/graphicSell.svg`,
+		strong_sell: `${__dirname}/../assets/images/graphicStrongSell.svg`,
 	},
 };
